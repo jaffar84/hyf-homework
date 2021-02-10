326b941339786baf0f2973c9127d9110d9f4961d
Jaafar Juneidi <69820109+jaffar84@users.noreply.github.com>
2021-02-10 03:48:47 +0100

    Delete javascript/javascript1/week1 directory

--- javascript/javascript1/week1/agefi.js
+++
@@ -1,4 +0,0 @@
-var yearOfbirth = 1984;
-var yearFuture = 2030;
-var age = yearFuture - yearOfbirth;
-console.log ('may age in 2030 willbe', age);
--- javascript/javascript1/week1/test2.js
+++
@@ -1,2 +0,0 @@
-var myName = 'jaafar';
-console.log (myName);
