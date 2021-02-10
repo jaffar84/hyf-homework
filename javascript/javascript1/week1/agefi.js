d4cf146ec3e4633a06f415376d96bd3e453f86db
Jaafar Juneidi <69820109+jaffar84@users.noreply.github.com>
2021-02-10 05:40:55 +0100

    finished

---
+++ javascript/javascript1/week1/agefi.js
@@ -0,0 +1,4 @@
+var yearOfbirth = 1984;
+var yearFuture = 2030;
+var age = yearFuture - yearOfbirth;
+console.log ('may age in 2030 willbe', age);
---
+++ javascript/javascript1/week1/test2.js
@@ -0,0 +1,2 @@
+var myName = 'jaafar';
+console.log (myName);
